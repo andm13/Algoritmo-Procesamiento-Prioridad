85115f9110f0a3eec705014c89ddad83c9ef6722
andm13 <andm13@github.com>
2019-12-22 12:04:15 -0500

    cola se ordena

--- main.py
+++ main.py
@@ -31,15 +31,39 @@ for x in range(num_procesos):
     pass
     print ""
 total=0
+#tiempo de ejecucion total
 for x in range(num_procesos):
     total= total+procesos[x].t_proceso
     pass
 #inicio de algoritmo
 for x in range(total):
+    for y in range(num_procesos):
+        if procesos[y].t_inicio==x:
+            cola.append(procesos[y])
+            if len(cola)>1:
+                for z in range(len(cola)-1):
+                    if(cola[z].prioridad>cola[z+1].prioridad):
+                        aux = cola[z]
+                        cola[z]=cola[z+1]
+                        cola[z+1]=aux
+                        pass
+                    pass
+                pass
+        pass
+    for y in range(num_procesos):
+        if procesos[y] not in cola:
+            procesos[y].estado.append(0)
+            pass
+        else:
+
+        pass
+    pass
+"""
+for x in range(total):
     print x
     for y in range(num_procesos):
         if procesos[y] in cola:
-
+            procesos[y].estado.append(1)
             pass
         else:
             procesos[y].estado.append(0)
@@ -53,10 +77,33 @@ for x in range(total):
                     if(cola[z].prioridad>cola[z+1].prioridad):
                         aux = cola[z]
                         cola[z]=cola[z+1]
-                        colaz[z+1]=aux
+                        cola[z+1]=aux
                         pass
                 pass
             pass
+            if cola[0].t_inicio != x:
+                pro = procesos.index(cola[0])
+                cola[0].t_proceso -=1
+                procesos[pro].estado.append(1)
+                pass
+            if cola[0].t_inicio ==0:
+                pro=procesos.index(cola[0])
+                cola.pop(0)
+                procesos[pro].t_finaliza=x;
         pass
-
     pass
+"""
+
+#estado 0= no hace nada
+#estado 1= ejecuta
+#estado 2= espera
+
+#impresion de datos
+for x in range(num_procesos):
+    print "Proceso "+str(x+1)
+    print "Tiempo t_finaliza: "+str(procesos[x].t_finaliza)
+    print "Tiempo procesamiento: "+str(procesos[x].t_proceso)
+    print "Prioridad: "+str(procesos[x].prioridad)
+    print procesos[x].estado
+    pass
+    print ""
