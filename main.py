537dd7d1e1359c3f7d2314e1c69b1ccd1f7ef76e
andm13 <andm13@github.com>
2019-12-28 09:23:01 -0500

    crea el diagrama correctamente y realiza el algoritmo

--- main.py
+++ main.py
@@ -36,7 +36,19 @@ for x in range(num_procesos):
     total= total+procesos[x].t_proceso
     pass
 #inicio de algoritmo
-for x in range(total):
+for x in range(total+1):
+
+    for y in range(num_procesos):
+        if procesos[y] not in cola:
+            procesos[y].estado.append(0)
+            pass
+        elif procesos[y]==cola[0] and procesos[y].t_inicio!=x:
+            procesos[y].estado.append(1)
+            cola[0].t_proceso-=1
+        elif (procesos[y] in cola) and (procesos[y] != cola[0]):
+            procesos[y].estado.append(2)
+            pass
+    pass
     for y in range(num_procesos):
         if procesos[y].t_inicio==x:
             cola.append(procesos[y])
@@ -50,57 +62,13 @@ for x in range(total):
                     pass
                 pass
         pass
-    for y in range(num_procesos):
-        if procesos[y] not in cola:
-            procesos[y].estado.append(0)
+    if cola:
+        if cola[0].t_proceso==0:
+            lugar= procesos.index(cola[0])
+            procesos[lugar].t_finaliza=x
+            cola.pop(0)
             pass
-        elif procesos[y]==cola[0]:
-            procesos[y].estado.append(1)
-            cola[0].t_proceso-=1
-            if cola[0].t_proceso==0:
-                cola.pop(0)
-                procesos[y].t_finaliza=x+1
-                pass
-            pass
-        elif (procesos[y] in cola) and (procesos[y] != cola[0]):
-            procesos[y].estado.append(2)
-            pass
-    pass
-"""
-for x in range(total):
-    print x
-    for y in range(num_procesos):
-        if procesos[y] in cola:
-            procesos[y].estado.append(1)
-            pass
-        else:
-            procesos[y].estado.append(0)
         pass
-    for y in range(num_procesos):
-        if procesos[y].t_inicio==x:
-            cola.append(procesos[y])
-            procesos[y].estado.append(0)
-            if(len(cola)!=1):
-                for z in range(len(cola)-1):
-                    if(cola[z].prioridad>cola[z+1].prioridad):
-                        aux = cola[z]
-                        cola[z]=cola[z+1]
-                        cola[z+1]=aux
-                        pass
-                pass
-            pass
-            if cola[0].t_inicio != x:
-                pro = procesos.index(cola[0])
-                cola[0].t_proceso -=1
-                procesos[pro].estado.append(1)
-                pass
-            if cola[0].t_inicio ==0:
-                pro=procesos.index(cola[0])
-                cola.pop(0)
-                procesos[pro].t_finaliza=x;
-        pass
-    pass
-"""
 
 #estado 0= no hace nada
 #estado 1= ejecuta
@@ -115,3 +83,9 @@ for x in range(num_procesos):
     print procesos[x].estado
     pass
     print ""
+
+
+#imprime diagrama
+for x in range(num_procesos):
+    print procesos[x].estado
+    pass
